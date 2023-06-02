dada03b420d239090fd2334e795abe6263710078
Edmundo <brayanedmundo.08@gmail.com>
2023-06-02 10:55:32 -0600

    Componente de Login "Agregado"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import Content from './components/Content';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Aside from './components/Aside';
+// import Content from './components/Content';
+// import Footer from './components/Footer';
+// import Header from './components/Header';
+// import Aside from './components/Aside';
+import Login from './components/login';
 
 function App() {
   return (
     <>
-      <Header />
-      <Content />
-      <Footer />
-      <Aside />
+      
+      <Login />  
     </>
   );
 }
