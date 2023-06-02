648d33fcfed28b6534772918fa5efedcb0b38a7c
Edmundo <brayanedmundo.08@gmail.com>
2023-06-02 11:28:39 -0600

    Datos Tablas Agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import Content from './components/Content';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Aside from './components/Aside';
+// import Content from './components/Content';
+// import Footer from './components/Footer';
+// import Header from './components/Header';
+// import Aside from './components/Aside';
+import DataTable from './components/dataTable';
 
 function App() {
   return (
     <>
-      <Header />
-      <Content />
-      <Footer />
-      <Aside />
+    <DataTable />
     </>
   );
 }
